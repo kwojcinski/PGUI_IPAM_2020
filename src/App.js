1a0c1a1de631268b5c4e557a5c2ab6d584f950d7
Karol Wójciński <karol.wojcinski@communitycert.org>
2020-01-25 16:52:14 +0100

    auth0

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+
+// New - import the React Router components, and the Profile page component
+import { Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./components/PrivateRoute";
+import Profile from "./components/Profile";
+import history from "./utils/history";
+
+function App() {
+  return (
+    <div className="App">
+      {/* Don't forget to include the history module */}
+      <Router history={history}>
+        <header>
+          <NavBar />
+        </header>
+        <Switch>
+          <Route path="/" exact />
+          <PrivateRoute path="/profile" component={Profile} />
+        </Switch>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
