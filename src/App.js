d9963e469568a3276da3a94a5a6c3297df481e41
bzadrozny <barzadrozny@gmail.com>
2020-01-25 17:54:28 +0100

    homepage + moving to named dictionaries

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
 import React from "react";
-import NavBar from "./components/NavBar";
+import NavBar from "./components/public/NavBar";
 
 // New - import the React Router components, and the Profile page component
 import { Router, Route, Switch } from "react-router-dom";
 import PrivateRoute from "./components/PrivateRoute";
-import Profile from "./components/Profile";
+import Profile from "./components/private/Profile";
 import history from "./utils/history";
+import Homepage from "./components/public/Homepage";
 
 function App() {
   return (
     <div className="App">
       {/* Don't forget to include the history module */}
       <Router history={history}>
+
         <header>
           <NavBar />
         </header>
-        <Switch>
-          <Route path="/" exact />
-          <PrivateRoute path="/profile" component={Profile} />
-        </Switch>
+
+        <main>
+          <Switch>
+            <Route path="/" exact render={Homepage} />
+            <PrivateRoute path="/profile" component={Profile} />
+          </Switch>
+        </main>
+
       </Router>
     </div>
   );
