ebbdb348f067da7eadd18abaa529f4780249fd48
bzadrozny <barzadrozny@gmail.com>
2020-01-29 14:38:55 +0100

    structure

--- src/App.js
+++ src/App.js
@@ -3,10 +3,10 @@ import React from "react";
 // New - import the React Router components, and the Profile page component
 import {Router, Route, Switch} from "react-router-dom";
 import PrivateRoute from "./components/private/PrivateRoute";
-import RegisterDevice from "./components/private/RegisterDevice";
+import RegisterDevice from "./components/private/device/RegisterDevice";
 import VLANPage from "./components/private/vlan/VLANPage";
-import DefineNAT from "./components/private/DefineNAT";
-import Profile from "./components/private/Profile";
+import DefineNAT from "./components/private/nat/DefineNAT";
+import Profile from "./components/private/profile/Profile";
 import Homepage from "./components/public/Homepage";
 import NavBar from "./components/public/NavBar";
 import history from "./utils/history";
